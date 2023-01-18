5182b74164bcca7a2859bfb72b4fa4a2cd3fa6c6
Di <dias.ospanov@gmail.com>
2023-01-18 21:08:35 +0200

    Create default meme and generate user meme.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 
 export default function App() {
-  const defaultMeme = (
-    <img
-      alt="Puffin"
-      src="https://api.memegen.link/images/puffin/I_don't_find_Mitch_Hedberg/all_the_funny.png"
-    />
+  // const defaultMeme = (
+  //   <img
+  //     alt="Puffin"
+  //     src="https://api.memegen.link/images/puffin/I_don't_find_Mitch_Hedberg/all_the_funny.png"
+  //   />
+  // );
+  const [meme, setMeme] = useState(
+    'https://api.memegen.link/images/puffin/.jpg',
   );
-  const [meme, setMeme] = useState(defaultMeme);
+  const [userMeme, setUserMeme] = useState('');
+  // const onClick = ({ target: { value } }) =>
+  //   setMeme('https://api.memegen.link/images/' + value + '/.jpg');
+  //   const onChange = ({ target: { value } }) =>
   return (
     <body
       style={{
@@ -18,7 +24,22 @@ export default function App() {
     >
       <h1>A Meme Generator</h1>
       <div>
-        <button>Generate</button>
+        <button
+          onClick={() => {
+            setMeme('https://api.memegen.link/images/' + userMeme + '/.jpg');
+          }}
+        >
+          Generate
+        </button>
+        {/* <TextBox type="email" onChange={onChange} value={email} /> */}
+        <input
+          style={{ margin: 5 }}
+          type=""
+          value={userMeme}
+          onChange={(event) => {
+            setUserMeme(event.currentTarget.value);
+          }}
+        />
       </div>
       <label>
         Top text
@@ -30,17 +51,19 @@ export default function App() {
       </label>
       <label>
         Meme template
-        <input style={{ margin: 5 }} type="" />
+        {/* <TextBox type="email" onChange={onChange} value={email} /> */}
+        <input
+          style={{ margin: 5 }}
+          type=""
+          // onKeyDown={onChange}
+          // value={templateMeme}
+        />
       </label>
       <div>
         <button>Download</button>
       </div>
       <div>
-        <img
-          data-test-id="meme-image"
-          alt="A generated meme"
-          src="https://api.memegen.link/images/puffin/I_don't_find_Mitch_Hedberg/all_the_funny.png"
-        />
+        <img data-test-id="meme-image" alt="A generated meme" src={meme} />
       </div>
     </body>
   );
